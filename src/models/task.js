6c062587be80349896791c9faec636bab5ca516b
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-08-25 12:29:22 +0500

    added task model

---
+++ src/models/task.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+module.exports = Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
