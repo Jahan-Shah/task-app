0ccd407480089b81372a8d64852e029f0a290d36
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-08-29 09:50:12 +0500

    added sharp to set size for avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const sharp = require("sharp");
 const router = new express.Router();
 const auth = require("../middleware/auth");
 const User = require("../models/user");
@@ -99,7 +100,11 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -124,7 +129,7 @@ router.get("/users/:id/avatar", async (req, res) => {
 
     if (!user || !user.avatar) throw new Error();
 
-    res.set("Content-Type", "image/jpg");
+    res.set("Content-Type", "image/png");
     res.send(user.avatar);
   } catch (err) {
     res.status(404).send();
