ddc6a9ed8f6538814cc929d2d569321e07490fd5
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-08-29 16:28:37 +0500

    added missing file for deleting task with user

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema(
   {
@@ -105,7 +106,6 @@ userSchema.pre(
   async function (next) {
     const user = this;
     await Task.deleteMany({ owner: user._id });
-
     next();
   }
 );
